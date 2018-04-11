f8d28e3a4798f0aacd46f8f1742b5e319581d4dd
Johannes Voet <johannes.voet@voet-it.com>
2018-04-11 12:40:11 +0200

    Attached debugger

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+var debug = require('debug')('test:router');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  debug("/ get performed");
   res.render('index', { title: 'Express' });
 });
 
